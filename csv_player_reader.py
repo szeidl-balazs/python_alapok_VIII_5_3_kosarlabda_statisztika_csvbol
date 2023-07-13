b4071edb3e9097701a36a49da89346a2449ea2b8
bszeidl <szeidl.balazs@hotmail.com>
2023-07-13 15:39:22 +0200

    The values with which there will be calculations have been converted from string to integer.

--- csv_player_reader.py
+++ csv_player_reader.py
@@ -6,20 +6,27 @@ def read_csv_player(file_name_2):
     player_list = []
 
     try:
+        #open the csv file:
         player_file = open(file_name_2)
 
         try:
+            #player_content will store the content of the csv, the content is read in by the reader() function of csv module
             player_content = csv.reader(player_file)
 
+            #append the csv each row to an array list []
             for row in player_content:
                 player_list.append(row)
-
+                
+            #delete the header of the list      
+            del player_list[0]
+            
 
         finally:
             player_file.close()
            
 
     except OSError:
+        #if the input file name is wrong drops an error message
         sys.exit('Hibás file név!')
 
     else:
--- csv_score_reader.py
+++ csv_score_reader.py
@@ -7,13 +7,23 @@ def read_csv_score(file_name_1):
   
 
     try:
+        #open the csv file:
         score_file = open(file_name_1)        
 
         try:
+            #score_content will store the content of the csv, the content is read in by the reader() function of csv module
             score_content = csv.reader(score_file)
             
             for row in score_content:
+                 #append the csv each row to an array list []
                 score_list.append(row)
+
+            #delete the header of the list 
+            del score_list[0]
+
+            #converts the score value from string to integer to calculate with that
+            for row in score_list:
+                row[2] = int(row[2])
 
         finally:
             score_file.close()
