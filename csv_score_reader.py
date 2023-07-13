f4037298e6ef77a0c933c10c21d0a3d55e6610e1
bszeidl <szeidl.balazs@hotmail.com>
2023-07-13 15:53:06 +0200

    String to integer converter is outsourced to a new function in a new file module, the code runs.

--- csv_score_reader.py
+++ csv_score_reader.py
@@ -21,10 +21,7 @@ def read_csv_score(file_name_1):
             #delete the header of the list 
             del score_list[0]
 
-            #converts the score value from string to integer to calculate with that
-            for row in score_list:
-                row[2] = int(row[2])
-
+            
         finally:
             score_file.close()
            
@@ -32,7 +29,5 @@ def read_csv_score(file_name_1):
         sys.exit('Hibás file név!')
 
     else:
-        print(score_list)
         return score_list
     
-read_csv_score('lakers_heats_final.csv')
